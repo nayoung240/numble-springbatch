6633c3e80869791859ccfa80baf1ca1bb4ccce03
zerome <iirony@naver.com>
2023-06-30 23:06:12 +0900

    프로젝트 초기 생성

---
+++ src/main/java/com/example/numblespringbatch/NumbleSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.numblespringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleSpringbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleSpringbatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/numblespringbatch/NumbleSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numblespringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleSpringbatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
