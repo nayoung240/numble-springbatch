b01875a41fc9b0b8c37faf8e36ca120fb7d00117
zerome <iirony@naver.com>
2023-07-02 16:36:44 +0900

    h2-console, favicon 검증 제외

--- src/main/java/com/example/numblespringbatch/config/SecurityConfig.java
+++ src/main/java/com/example/numblespringbatch/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package com.example.numblespringbatch.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 하위 모든 요청들, 파비콘 관련 요청은 Spring Security 로직을 수행하지 않는다
+        return (web) -> web.ignoring().antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
